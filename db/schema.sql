780d291056db8ba11bf5a1e605db94cbdad3fe87
Maria Meyer <mbean1216@icloud.com>
2022-05-05 12:27:01 -0500

    starting files and folders

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS tech_talks_db;
+
+CREATE DATABASE tech_talks_db;
+
+USE tech_talks_db;
---
+++ server.js
@@ -0,0 +1,9 @@
+// TODO:
+// create and connect to db
+// create express session that times out after certain period of time
+// create user model?
+// create blog model?
+// create create, read, update, and delete routes for user model
+// create login/sign up for user
+// create validation for username and password
+// create html
